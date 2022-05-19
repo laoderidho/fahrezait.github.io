18b2ec6c94f7d891943305c446fd0a5840c5305d
laoderidho <2081007@unai.edu>
2022-05-19 22:56:09 +0700

    perubahan lagi
    Ini adalah perubahan untuk materi saaat ini

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
-// ini hanya Untuk tulisan di dalam ID
+// ini hanya Untuk tulisan di dalam ID jika ingin  tahu maka lihat id du html
 document.getElementById("ini").innerHTML="Ini adalah Ridho";
 
-// untype javascript bisa mengubah variabel di mana variabel yang baru yang akan di input
+// untype javascript bisa mengubah variabel di mana variabel yang baru yang akan di input 
+// karena var sudah tidak valid lagi maka ada let
 
-var x = 1234;
+let x = 1234;
     x =" String";
 
-    document.writeln(x);
+    document.getElementById("ini2").innerHTML=(x);
 
-// tanda tambah di perlukan untuk membuat penambahan string untuk string 
+// const dalam javascript adalah suatu variabel yang tidak bisa di ganti / tidak bisa untype
+// karena variabel const adalah tetap dan error jika variabel nya di ganti 
+// contoh 
 
-document.getElementById("ini2").innerHTML="Ini adalah Ridho" + " " + "fahreza";
+const v = 1234;
+    //   v = "String"; di comment karena error jika tidak percaya maka uncoment aja
+
+      document.writeln(v);
+
+// tanda tambah di perlukan untuk membuat penambahan string untuk tipe data  string 
+
+document.getElementById("ini3").innerHTML="Ini adalah Ridho" + " " + "fahreza";
+
+document.writeln("<br><br><br>")
+// Operator Aritmatika 
+
+let segitiga = 1/2*3*3;
+
+    segitiga = 
+
+document.writeln("luas segitiga berikut adalah" +" "+ segitiga);

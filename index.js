3d1c2210d24bf54f82a5a95f6aede46eb7efd213
laoderidho <2081007@unai.edu>
2022-05-19 21:43:26 +0700

    Pengubahan ke dua
    meteri ke dua

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 // ini hanya Untuk tulisan di dalam ID
-document.getElementById("ini").innerHTML="Ini adalah Ridho"
+document.getElementById("ini").innerHTML="Ini adalah Ridho";
+
+// untype javascript bisa mengubah variabel di mana variabel yang baru yang akan di input
+
+var x = 1234;
+    x =" String";
+
+    document.writeln(x);
+
+// tanda tambah di perlukan untuk membuat penambahan string untuk string 
+
+document.getElementById("ini2").innerHTML="Ini adalah Ridho" + " " + "fahreza";

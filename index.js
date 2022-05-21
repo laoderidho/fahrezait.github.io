5f5ee77eda15507dedb0b0b6ac9f33966d67473b
laoderidho <2081007@unai.edu>
2022-05-21 10:19:20 +0700

    pengubahan
    pengubahan materi

--- index.js
+++ index.js
@@ -26,7 +26,101 @@ document.writeln("<br><br><br>")
 // Operator Aritmatika 
 
 let segitiga = 1/2*3*3;
+ 
 
-    segitiga = 
+document.writeln("luas segitiga berikut adalah" +" "+ segitiga+"<br>");
 
-document.writeln("luas segitiga berikut adalah" +" "+ segitiga);
+// operasi augmented
+
+let hitung = 12;
+
+hitung+= 10;
+
+document.writeln("hasil hitung 12+ 10 =" + hitung +"<br>"); //ini adalah hasil hitung sama seperti 
+// hitung = 10+2;
+
+// operator unary adalah operator yang menggunaka penambahan angka 
+// contoh 
+
+let result = +1;
+
+    result ++;
+
+    document.getElementById("ini4").innerHTML="hasil dari unary berikut adalah" + " "+ result;
+
+// operasi perbandingan 
+
+let hitung1 = 12 =="12";
+
+    document.writeln(hitung1); //berniai true / benar jika perbandingan nilai memang sama 
+
+let hitung2 = 12==="12";
+
+    document.writeln(hitung2); //bernilai false karena tipe data tidak sama. 
+    // berbeda dengan yang tadi jika simbol = ada 3 kali maka lebih spesifik tipe datanya 
+
+// Operator Logika adalah operator perbandingan menggunakan logika matematika 
+// ada yang di namakan && (and), || (or/ atau), ! (negasi atau tidak sama dengan)
+// operator tersebut akan bernilai seperti coding di bawah ini 
+
+const nilaiujian = 70;
+const nilaiabsensi = 80;
+
+const lulusujian = nilaiujian >=70;
+const lulusabsensi = nilaiabsensi >=70;
+
+const hasil = lulusujian && lulusabsensi;
+
+    document.getElementById("ini5").innerHTML= "jika bernilai true maka lulus kali ini "+hasil;
+    // hasil tersebut true jika lulus ujian dan lulus absensi bernilai true 
+    // jika salah satu hasil bernilai false maka nilai berifat false 
+
+    const nilaiuji = 30;
+    const nilaiabsen = 80;
+    
+    const lulusuji = nilaiuji >=70;
+    const lulusabsen = nilaiabsen >=70;
+    
+    const hitung3 = lulusuji || lulusabsen;
+    document.getElementById("ini6").innerHTML= "jika bernilai true maka lulus kali ini "+hitung3;
+
+    // hasil tersebut bernilai true karena salah satu dari elemen tersbut bernilai true 
+    // jika hasil tersebut dua duanya false maka hasilnya false 
+
+    const aku1 = 30;
+    const aku2 = 50;
+
+    const saya1= aku1 != aku2;
+
+    document.writeln(saya1);
+
+    // bernilai true jika nilai saling false satu sama lain 
+
+// console adalah suatu debug atau hasil dari program anda tanpa mengganggu user 
+// hasil dari console akan muncul jika kita mengklik inspeksi dalam web browser kita
+
+console.log("Hello word");
+console.info("Hello Ridho");
+console.warn("ini peringatan"); //untuk pengingat
+console.error("ini error"); //jika untuk hal errror
+
+// string template menggunakan tanda backtip `` dalam kodenya untuk menambahkan variabel
+// sama seperti tanda + untuk menambahkan string tapi ini lebih memakan sedikit waktu 
+// contoh 
+
+const namadepan= "La Ode";
+const namatengah= "Ridho";
+const namabelakang ="Fahreza";
+const nilai = 80;
+const namalengkap = `"Nama Lengkap = " ${namadepan} ${namatengah} ${namabelakang} Nilai ${nilai>70}`;
+
+console.log(namalengkap);
+
+const pre = `saya adalah Ridho
+mahasiswa di Unai
+Dan kerja Sebagai It`; //hanya berlaku jika memakai tanda backtip (``) jika memakai tanda kutip 2 ("")
+// maka tidak bisa
+
+document.getElementById("inipre").innerHTML= pre;
+
+
